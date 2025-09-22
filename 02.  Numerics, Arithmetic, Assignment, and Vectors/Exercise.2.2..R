ef4615adce63bb310be8b83c3dd4ea6b50cb671f
unknown <sly_racoon87@abv.bg>
2025-09-22 11:01:47 +0300

    Exercise with variables.

---
+++ 02.  Numerics, Arithmetic, Assignment, and Vectors/Exercise.2.2..R
@@ -0,0 +1,20 @@
+#a. Create an object that stores the value 3^2 * 4^1/8
+
+a <- 3^2 * 4^(1/8)
+
+#b. Overwrite your object in (a) by itself divided by 2.33.
+# Print the result to the console.
+
+a <- a / 2.33
+print(a)
+
+
+#c. Create a new object with the value (-8.2) * 10^(-13).
+
+b <- (-8.2) * 10^(-13)
+
+#d. Print directly to the console the result of multiplying (b) by (c).
+
+print(a*b)
+
+
